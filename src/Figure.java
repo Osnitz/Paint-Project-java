76518b381529089aea4d9c997e6f120ac0a477a9
Osnitz <lolmatt18@gmail.com>
2023-11-15 09:53:54 +0100

    15/11/2023

---
+++ src/Figure.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+abstract class Figure {
+    protected Color c;
+
+    public abstract void setBoundingBox(int heightBB, int widthBB);
+
+    public abstract void draw(Graphics g);
+
+    public abstract void Figure();
+    public abstract void getSurface();
+
+
+
+}
--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,39 @@
 public class Point {
+    public int X;
+    public int Y;
+    public Point(){
+        this.X = 0;
+        this.Y = 0;
+    }
 
+    @Override
+    public String toString() {
+        return ("("+ X + " ; "+ Y+")");
+    }
+
+    public int getX(){
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setX(int x){
+        X = x;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public static void main (String[] args){
+        Point a = new Point();
+        String stringA = a.toString();
+        System.out.println("coordonées de a :" +stringA);
+        a.setX(2);
+        a.setY(3);
+        stringA = a.toString();
+        System.out.println("coordonées de a :" + stringA);
+    }
 }
